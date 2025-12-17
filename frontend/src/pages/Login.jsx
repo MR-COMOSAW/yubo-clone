731cf2a31c4daa95b28cbcccbdade3e757d996d2
MR-COMOSAW <buskalex34@gmail.com>
2025-12-17 20:07:07 +0100

    Delete frontend directory

--- frontend/src/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-
-import axios from "axios";
-export default function(){
-  const login=async()=>{
-    const r=await axios.post(import.meta.env.VITE_API+"/api/auth/login",{email:"test@test.se",password:"123"});
-    localStorage.token=r.data.token;
-  };
-  return <div className="box"><h2>Login</h2><button onClick={login}>Login</button></div>;
-}
